ba38df73ca5404ebd679421b3e66f97e8a38ce1e
motionbit95 <motionbit.dev@gmail.com>
2024-01-29 23:53:07 +0900

    default

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-﻿var express = require("express");
+﻿var express = require('express');
 var app = express();
-var path = require("path");
-var bodyParser = require("body-parser");
-var cors = require("cors");
-app.use(bodyParser.urlencoded({ extended: true }));
+var path = require('path');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true}));
 
-const PORT = process.env.PORT || 8000;
-const dev = false;
-const url = dev
-  ? `http://localhost:${PORT}`
-  : "https://dinnermate-node-server-0d7d5dc74685.herokuapp.com";
-app.use(
-  cors({
-    origin: [
-      `http://localhost:${PORT}`,
-      "https://dinnermate-node-server-0d7d5dc74685.herokuapp.com/",
-    ],
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
+var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
 
-var indexRouter = require("./routes/index");
-app.use("/", indexRouter);
+app.use('/public', express.static(__dirname + '/public'));
 
-app.use("/public", express.static(__dirname + "/public"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`start! express server on ${url}`);
+  console.log(`start! express server on http://localhost:${PORT}`);
 });
